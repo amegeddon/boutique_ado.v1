f316d05a3d6f2f6c8ce3362c7108acb29f9ea878
amegeddon <ameescook@gmail.com>
2024-08-06 21:43:25 +0100

    Completed home page and header

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -128,6 +128,11 @@ DATABASES = {
 # settings.py
 
 # URL prefix for static files
+
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
