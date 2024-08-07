53a91a77401ae62ac1c3d1d6da47df9cf549ddc2
amegeddon <ameescook@gmail.com>
2024-08-07 13:33:46 +0100

    Adds data for products

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -91,6 +91,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'home',
+    'products',
 ]
 
 MIDDLEWARE = (
@@ -134,5 +135,9 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
